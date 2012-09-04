520ca6eecf7ed16abaadbef0d0c6e67576810283
Vladimir Sabev <vdsabev@gmail.com>
2012-09-05 01:34:34 +0300

    Fixed the asses pipeline, added some style

--- Gemfile
+++ Gemfile
@@ -1,13 +1,13 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
-
+gem 'bootstrap-sass', '2.0.4'
 gem 'jquery-rails', '2.0.2'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', '3.2.5'
+  gem 'sass-rails', '3.2.4' # 3.2.5
   gem 'coffee-rails', '3.2.2'
   gem 'uglifier', '1.2.3'
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "Tag It | Home")
     end
   end
