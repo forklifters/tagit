873065840cbff84d3807c4a95f5a734af9be60c3
Vladimir Sabev <vdsabev@gmail.com>
2012-09-04 23:12:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TagitRails::Application.config.secret_token = 'f36128cad832ed9a1dcfb1af51cdecf1734e6d0b8fffbafaf39aee4cc72e491e0cbc2dba007004939d7871bd5f97426f891c28c0dd58334c019bf94cee07e915'
