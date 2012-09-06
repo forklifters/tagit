c6a8105d1565d6aa1b1635640ab4befa7e96c5f1
Vladimir Sabev <vdsabev@gmail.com>
2012-09-07 02:00:53 +0300

    Refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
-    redirect_to signin_path unless signed_in?
+    redirect_to signin_path and return unless signed_in?
+    @post = current_user.posts.build
+    @posts = current_user.posts.paginate(page: params[:page])
   end
 end
