092991afa81bff1573394bfbe07c4b6c324e8777
Vladimir Sabev <vdsabev@gmail.com>
2012-09-18 01:30:26 +0300

    Readded admin_data

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,4 +1,4 @@
 AdminData.config do |config|
-  config.is_allowed_to_view = lambda {|controller| controller.send('admin_signed_in?') }
-  config.is_allowed_to_update = lambda {|controller| controller.send('admin_signed_in?') }
+  config.is_allowed_to_view = lambda {|controller| controller.send('current_user.admin?') }
+  config.is_allowed_to_update = lambda {|controller| controller.send('current_user.admin?') }
 end
